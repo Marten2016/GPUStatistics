0382753bd7f6804f8f61a50281274eb8dcc7a478
Marten2016 <595384873@qq.com>
2023-08-23 10:25:36 +0800

    Update GPUDeviceStat.py

--- GPUDeviceStat.py
+++ GPUDeviceStat.py
@@ -1,11 +1,18 @@
 import os
+import sys
 import time
 
 str_command = "nvidia-smi"
-out_path = "gpu_device_stat_result.txt"
+out_max_path = "./gpu_device_stat_max_result.txt"
+out_min_path = "./gpu_device_stat_min_result.txt"
+out_avg_path = "./gpu_device_stat_avg_result.txt"
+
 time_interval = 5
 
-result_map = dict()
+result_max_map = dict()
+result_min_map = dict()
+result_avg_map = dict()
+avg_count = 0
 
 while True:
 
@@ -14,27 +21,69 @@ while True:
     out.close()
     
     content_0 = text_content.split("|===============================+======================+======================|")
-    #content_1 = content_0[1].split("+-------------------------------+----------------------+----------------------+\n                                                                               \n+-----------------------------------------------------------------------------+")
-    
     lines = content_0[1].split("+-------------------------------+----------------------+----------------------+")
     
-    update_tag = False
+    update_max_tag = False
+    update_min_tag = False
+    update_avg_tag = False
+    avg_count += 1
+
     for i in range(len(lines)):
         if i >= len(lines)-1:
             break
+        #modify "Tesla V100S-PCI" to real type of nvidia
         device = lines[i].split("  Tesla V100S-PCI...  Off")[0].split(" ")[-1]
         #device = lines[i].split("  Tesla V100-PCIE...  Off")[0].split(" ")[-1]
-        mem = lines[i].split("MiB / ")[0].split(" ")[-1]
-        if device in result_map:
-            if result_map[device] < mem:
-                result_map[device] = mem
-                update_tag = True
+        mem = int(lines[i].split("MiB / ")[0].split(" ")[-1])
+        #update max mem
+        if device in result_max_map:
+            if result_max_map[device] < mem:
+                result_max_map[device] = mem
+                update_max_tag = True
         else:
-            result_map[device] = mem
-            update_tag = True
-    if update_tag:
-        fout = open(out_path, "w")
-        fout.write(str(result_map))
-        fout.close()
-        update_tag = False
+            result_max_map[device] = mem
+            update_max_tag = True
+        #update min mem
+        if device in result_min_map:
+            if result_min_map[device] > mem:
+                result_min_map[device] = mem
+                update_min_tag = True
+        else:
+            result_min_map[device] = mem
+            update_min_tag = True
+        #update avg mem
+        #mem_float = float(mem)/1024
+        if device in result_avg_map:
+            prevalue = result_avg_map[device].split(":")
+            #if float(prevalue[0]) >= sys.float_info.max - 32 or avg_count >= sys.maxsize - 1:
+            if int(prevalue[0]) >= sys.maxsize - 32768 or avg_count >= sys.maxsize - 1:
+                print("too long to save, finish avg job!")
+                continue
+            #sum_value = float(prevalue[0]) + mem_float
+            sum_value = int(prevalue[0]) + int(mem)
+
+            #avg_count += 1
+            result_avg_map[device] = ':'.join([str(sum_value), str(avg_count)])
+            update_avg_tag = True
+        else:
+            #result_avg_map[device] = ':'.join([str(mem_float), str(avg_count)])
+            result_avg_map[device] = ':'.join([str(mem), str(avg_count)])
+            update_avg_tag = True
+    #write result to file    
+    if update_max_tag:
+        fxout = open(out_max_path, "w")
+        fxout.write(str(result_max_map))
+        fxout.close()
+        #update_max_tag = False
+    if update_min_tag:
+        fnout = open(out_min_path, "w")
+        fnout.write(str(result_min_map))
+        fnout.close()
+        #update_min_tag = False
+    if update_avg_tag:
+        fgout = open(out_avg_path, "w")
+        fgout.write(str(result_avg_map))
+        fgout.close()
+        #update_avg_tag = False
+    
     time.sleep(time_interval)
